1edc37b2146dc7c7f7478e9683f4c79f8159a747
Sumaiiya-Haque <sumaiyahaque475@gmail.com>
2025-11-17 12:40:33 +0600

    created banner with swiperjs

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Brands from '../Brands/Brands';
+
+const Home = () => {
+    return (
+        <div>
+            <Brands></Brands>
+           <Banner></Banner> 
+        </div>
+    );
+};
+
+export default Home;
